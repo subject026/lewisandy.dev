a769629a1cd2fa37d5493dfd28ad5186a7680a82
subject026 <subject026@protonmail.com>
2022-09-06 14:55:57 +0100

    solid config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  outDir: "./docs",
+  integrations: [tailwind(), solidJs()],
+  outDir: "./docs"
 });
